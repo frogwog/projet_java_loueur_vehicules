582319aa7a8fb93aaa5cc8a3bea7754e54b7006e
Edouard Soares <soares.edouard@gmail.com>
2016-03-18 12:48:12 +0100

    petit essai dans main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Eddy on 18/03/16.
  */
@@ -5,6 +8,32 @@ public class Main {
 
     public static void main(String [] args) {
 
-        System.out.println("Hello world !");
+        List e1 = new Exemplaire(1,12000);
+
+        List e2 = new Exemplaire(2,130000);
+
+        List v1 = new Auto("Audi","A8");
+
+        List emp1 = new Emprunteur(123,"Soares","Edouard","Paris");
+
+        List emp2 = new Emprunteur(124,"Garnier","Florian","Paris");
+
+
+        List maListe = new ArrayList();
+
+        maListe.add(e1);
+        maListe.add(e2);
+        maListe.add(v1);
+        maListe.add(emp1);
+        maListe.add(emp2);
+
+        for (int i = 0 ; i < maListe.size() ; i++) {
+
+            System.out.println(maListe.get(i));
+        }
+
+
+
+
     }
 }
